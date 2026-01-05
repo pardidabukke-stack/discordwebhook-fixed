e6e643de5208b5b7fb7ab24db0ced0a8f41a7acf
pardidabukke-stack <pardidabukke@gmail.com>
2026-01-05 13:54:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ client.on('messageCreate', (message) => {
       const embed = new EmbedBuilder()
         .setDescription('yahaha kehapus dan maaf menganggu kenyaman nya, mwah.')
         .setColor(0x40E0D0)
-        .setImage('https://tenor.com/view/kiss-blow-kiss-love-you-love-you-lots-kisses-gif-2119490846757697886');
+        .setImage('https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWF3MXpuMnc3cXZya3R2MXhxeGJvM3VsaTB5NWpoc2dsNWJqdjY1eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ytu2GUYbvhz7zShGwS/giphy.gif');
       message.channel.send({ embeds: [embed] }).then(sentMessage => {
         setTimeout(() => {
           sentMessage.delete().catch(console.error);
         }, 3000);
       }).catch(console.error);
-    }, 60000);
+    }, 10000); // 10 seconds
   }
 });
 
