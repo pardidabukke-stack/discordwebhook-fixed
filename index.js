a558e8a23003c6de404a5899ab8b45fe45639628
pardidabukke-stack <pardidabukke@gmail.com>
2026-01-05 14:37:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const express = require('express');
 
 const client = new Client({
@@ -19,12 +19,16 @@ client.on('messageCreate', (message) => {
   if (message.webhookId) {
     setTimeout(() => {
       message.delete().catch(console.error);
-      message.channel.send('yahaha kehapus dan maaf menganggu kenyaman nya, mwah.').then(sentMessage => {
+      const embed = new EmbedBuilder()
+        .setDescription('yahaha kehapus dan maaf menganggu kenyaman nya, mwah.')
+        .setColor(0x40E0D0)
+        .setImage('https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWF3MXpuMnc3cXZya3R2MXhxeGJvM3VsaTB5NWpoc2dsNWJqdjY1eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ytu2GUYbvhz7zShGwS/giphy.gif');
+      message.channel.send({ embeds: [embed] }).then(sentMessage => {
         setTimeout(() => {
           sentMessage.delete().catch(console.error);
         }, 7000);
       }).catch(console.error);
-    }, 60000); // 60 seconds
+    }, 60000);  // 60 seconds
   }
 });
 
@@ -41,4 +45,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
